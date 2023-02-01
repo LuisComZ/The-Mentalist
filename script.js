fea020e4dff969ddec402a6487f5fcf95c6cbc5b
Luiz <luizmay2@gmail.com>
2023-01-31 23:20:14 -0300

    Start button and some features added

--- script.js
+++ script.js
@@ -1,13 +1,33 @@
-var numeroSecreto = parseInt(Math.random() * 1001)
+alert("Bem vindo ao Mentalista. Tente advinhar o número secreto")
+const button = document.querySelector("header button")
+button.addEventListener("click", add)
 
-while (chute != numeroSecreto) {
-  var chute = prompt("Digite um número entre 0 e 1000.")
+function add() {
+  var numeroSecreto = parseInt(Math.random() * 1001) + 1
+  var x = 0
+  var tentativas = 10
 
-  if (chute == numeroSecreto) {
-    alert("Acertou ✅")
-  } else if (chute > numeroSecreto) {
-    alert("Errou... o número secreto é menor")
-  } else if (chute < numeroSecreto) {
-    alert("Errou... o número secreto é maior")
+  while (chute != numeroSecreto) {
+    tentativas = tentativas - 1
+    var chute = prompt("Digite um número entre 1 e 1000 ou -1 para cancelar")
+
+    if (chute == numeroSecreto) {
+      alert("Acertou ✅")
+    } else if (chute == -1) {
+      break
+    } else if (tentativas == x) {
+      alert(
+        "Que pena... você alcançou o número máximo de tentativas. Boa sorte na próxima!"
+      )
+      break
+    } else if (chute == x) {
+      alert("Erro. Por favor, insira um número maior do que 0.")
+    } else if (chute > numeroSecreto) {
+      alert("Errou... o número secreto é menor que " + chute)
+      alert("Você tem " + tentativas + " tentativas")
+    } else if (chute < numeroSecreto) {
+      alert("Errou... o número secreto é maior que " + chute)
+      alert("Você tem " + tentativas + " tentativas")
+    }
   }
 }
